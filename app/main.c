18b84158d526709bcf6ec1cdd657aac9db82ccba
Speredonka <sevast14@yandex.ru>
2023-11-25 02:03:10 +0300

    finised testing plan

--- app/main.c
+++ app/main.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "myfunc.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("%f", sq_exq_roots(0, 0, 0));
+    if (argc - 1 != 3)
+    {
+        printf("Аргументов функции должно быть три!\n");
+        return 1;
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[2]);
+    int c = atoi(argv[3]);
+
+    if (a == 0)
+    {
+        printf("Не является квадратным уравнением!\n");
+        return 1;
+    }
+
+    double *roots = sq_exq_roots(a, b, c);
+
+    printf("%.2lf ", roots[0]);
+    printf("%.2lf ", roots[1]);
+
+    free(roots);
+
+    return 0;
 }
--- app/myfunc.h
+++ app/myfunc.h
@@ -1,7 +1,7 @@
 #ifndef MYFUNC_H
 #define MYFUNC_H
 
-// int fibonachi(int num);
+int fibonachi(int num);
 
 double *sq_exq_roots(int a, int b, int c);
 
