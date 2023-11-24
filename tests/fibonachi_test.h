3b1177a92d9fb8414c185c47e596334e75dba1cf
Speredonka <sevast14@yandex.ru>
2023-11-25 02:29:51 +0300

    tests done

--- tests/fibonachi_test.h
+++ tests/fibonachi_test.h
@@ -6,62 +6,58 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <unistd.h>
+#include <math.h>
 
 extern "C"
 {
 #include "myfunc.h"
 }
 
-// TEST(fibonachiTest, num0)
-// {
-//     ASSERT_EQ(fibonachi(0), 1);
-//     ASSERT_EQ(fibonachi(1), 1);
-//     ASSERT_EQ(fibonachi(2), 2);
-// }
-
-// TEST(fibonachiTest, greather2)
-// {
-//     ASSERT_EQ(fibonachi(5), 5);
-//     ASSERT_EQ(fibonachi(10), 55);
-// }
-
-// TEST(fibonachiTest, negative)
-// {
-//     ASSERT_EQ(fibonachi(-1), 0);
-// }
-
-// TEST(fibonachiTest, inputFile)
-// {
-//     char *filename = (char *)malloc(sizeof(char) * 1024);
-//     // sprintf(filename, "%s/input321.txt", INPUTDIR);
-
-//     int fd = open(filename, O_RDONLY);
-//     free(filename);
-//     if (fd < 0)
-//     {
-//         ASSERT_EQ(errno, 0);
-//     }
-
-//     char *buf = (char *)malloc(sizeof(char) * 512);
-//     int readcount = read(fd, buf, 512);
-//     ASSERT_TRUE(readcount > 0);
-//     close(fd);
-
-//     int input = 0;
-//     int output = 0;
-//     int ret = sscanf(buf, "%d %d", &input, &output);
-//     free(buf);
-//     ASSERT_EQ(ret, 2);
-
-//     ret = fibonachi(input);
-//     ASSERT_EQ(ret, output);
-// }
-
-TEST(sq_exq_rootsTest, base)
+TEST(sq_exq_rootsTest, basic)
 {
-    double *roots = sq_exq_roots(0, 0, 0);
+    double *roots = sq_exq_roots(1, -3, 2);
+    ASSERT_EQ(roots[0], 2.0);
+    ASSERT_EQ(roots[1], 1.0);
+    free(roots);
+}
+
+TEST(sq_exq_rootsTest, discriminant_is_zero)
+{
+    double *roots = sq_exq_roots(1, -2, 1);
+    ASSERT_EQ(roots[0], 1.0);
+    ASSERT_EQ(roots[1], 1.0);
+    free(roots);
+}
+
+TEST(sq_exq_rootsTest, no_roots)
+{
+    double *roots = sq_exq_roots(1, 2, 3);
+    ASSERT_TRUE(isnan(roots[0]));
+    ASSERT_TRUE(isnan(roots[1]));
+    free(roots);
+}
+
+TEST(sq_exq_rootsTest, c_is_zero)
+{
+    double *roots = sq_exq_roots(2, 4, 0);
     ASSERT_EQ(roots[0], 0.0);
-    ASSERT_EQ(roots[1], 0.0);
+    ASSERT_EQ(roots[1], -2.0);
+    free(roots);
+}
+
+TEST(sq_exq_rootsTest, big_numbers)
+{
+    double *roots = sq_exq_roots(10000, 50000, 100000);
+    ASSERT_TRUE(isnan(roots[0]));
+    ASSERT_TRUE(isnan(roots[1]));
+    free(roots);
+}
+
+TEST(sq_exq_rootsTest, intergrated)
+{
+    double *roots = sq_exq_roots(fibonachi(10), fibonachi(1), fibonachi(4));
+    ASSERT_TRUE(isnan(roots[0]));
+    ASSERT_TRUE(isnan(roots[1]));
     free(roots);
 }
 
